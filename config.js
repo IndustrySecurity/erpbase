aaf5ff14484ab86da0fb13d66143b52aa26e357d
IndustrySecurity <30697286+IndustrySecurity@users.noreply.github.com>
2023-11-26 20:12:22 +0800

    add link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
     // baseURL: "https://detail/"+"api/",  // 使用服务器的后端
-    baseURL: "http://127.0.0.1:8000/" + "api/", // 使用本地的后端
+    baseURL: "http://erp_backend:8000/" + "api/", // 使用本地的后端
     // baseURL: "/api/", // 部署用的后端
 }
